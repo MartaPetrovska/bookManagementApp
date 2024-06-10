875830abba4fe84a5624606a27ecf026c02bdbb5
Marta <133788310+MartaPetrovska@users.noreply.github.com>
2024-06-10 21:18:09 +0300

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,15 @@
+public class Book{
+    public String title;
+    public String author;
+    public String pages;
+    public String cover;
+    
+
+    public Book(String title, String author, String pages, String cover){
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.cover = cover;
+    }
+
+}
